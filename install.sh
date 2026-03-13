974dd55731b0cd78008b38ec2687c7bbb44dc7cb
OshiroAka <victorgabrielgs05@gmail.com>
2026-03-13 20:15:14 -0300

    fix: sintaxe read -r -p

--- install.sh
+++ install.sh
@@ -571,6 +571,7 @@ else
     printf '%s\n' "$HYPR_BLOCK" >> "$HYPR_CONF"
     ok "Configurações do ShiraOS injetadas no hyprland.conf"
 figurações do ShiraOS injetadas no hyprland.conf"
+figurações do ShiraOS injetadas no hyprland.conf"
 figurado para o ShiraOS"
     else
         # Backup antes de mexer
