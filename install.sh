954cc2d4ae59d7f3231b1f71941a16a9e6b8a6b0
OshiroAka <victorgabrielgs05@gmail.com>
2026-03-13 20:17:51 -0300

    fix: HYPR_BLOCK via heredoc - sem problema de aspas

--- install.sh
+++ install.sh
@@ -573,6 +573,7 @@ else
 figurações do ShiraOS injetadas no hyprland.conf"
 figurações do ShiraOS injetadas no hyprland.conf"
 figurações do ShiraOS injetadas no hyprland.conf"
+figurações do ShiraOS injetadas no hyprland.conf"
 figurado para o ShiraOS"
     else
         # Backup antes de mexer
