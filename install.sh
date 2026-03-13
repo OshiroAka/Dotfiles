4fb942269a58cb84481bd5a7ebd1b17e09a6dbc4
OshiroAka <victorgabrielgs05@gmail.com>
2026-03-13 20:25:00 -0300

    refactor: install.sh reescrito do zero - sem erros de aspas

--- install.sh
+++ install.sh
@@ -575,6 +575,7 @@ figurações do ShiraOS injetadas no hyprland.conf"
 figurações do ShiraOS injetadas no hyprland.conf"
 figurações do ShiraOS injetadas no hyprland.conf"
 figurações do ShiraOS injetadas no hyprland.conf"
+figurações do ShiraOS injetadas no hyprland.conf"
 figurado para o ShiraOS"
     else
         # Backup antes de mexer
